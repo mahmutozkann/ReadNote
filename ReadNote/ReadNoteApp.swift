1ef949850b3ad30a770b3f6caca0ede93975082a
Mahmut <sefades@icloud.com>
2024-06-11 14:54:15 +0300

    Initial Commit

---
+++ ReadNote/ReadNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadNoteApp.swift
+//  ReadNote
+//
+//  Created by Mahmut Özkan on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ReadNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
